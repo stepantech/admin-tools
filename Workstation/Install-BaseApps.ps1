ddafd2452e74c0b1ae8e94622b028fd73835ebe9
Petr Štěpán <91780309+stepantech@users.noreply.github.com>
2023-02-19 20:04:09 +0100

    Add files via upload
    Install-BaseApps

---
+++ Workstation/Install-BaseApps.ps1
@@ -0,0 +1,8 @@
+﻿# Easy to generate scripts https://winstall.app/
+
+winget install --id=7zip.7zip -e
+winget install --id=Adobe.Acrobat.Reader.64-bit -e
+winget install --id=Microsoft.Office -e
+winget install --id=VideoLAN.VLC -e
+winget install --id=Microsoft.Teams -e
+winget install --id=Zoom.Zoom -e 
